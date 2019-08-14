d085241d66436f068c3001016a4015549b5ba440
unknown <klaverhenrik@gmail.com>
2019-08-14 20:38:21 +0200

    my mobilenet

--- index.js
+++ index.js
@@ -19,12 +19,12 @@
 
 import {IMAGENET_CLASSES} from './imagenet_classes.js';
 
-const MOBILENET_MODEL_PATH =
+const MOBILENET_MODEL_PATH = './mobilenet/model.json';
     // tslint:disable-next-line:max-line-length
-    'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
+    //'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
 
 const IMAGE_SIZE = 224;
-const TOPK_PREDICTIONS = 10;
+const TOPK_PREDICTIONS = 3;
 
 let mobilenet;
 const mobilenetDemo = async () => {
